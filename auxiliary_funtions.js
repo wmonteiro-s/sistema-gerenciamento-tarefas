662492603e63e0770d10c6d061f4444ff040e831
Wesley Monteiro <wmonteiro873@gmail.com>
2025-04-16 23:34:38 -0300

    :sparkles: feat: add read and write of tasks with fs/promises

--- auxiliary_funtions.js
+++ auxiliary_funtions.js
@@ -1,5 +1,24 @@
 import {readFile, writeFile} from 'fs/promises'
 
+const FILE_NAME = 'tasks.json'
+
+async function loadTasks() {
+    try {
+        const dados = await readFile(FILE_NAME, 'utf-8')
+        return JSON.parse(dados)
+    } catch (error) {
+        console.error('Erro ao carregar arquivo', error)
+    }
+}
+
+async function saveTasks(task) {
+    try {
+        await writeFile(FILE_NAME, JSON.stringify(task, null, 2))
+    } catch (error) {
+        console.error('Erro ao salvar tarefa', error)
+    }
+}
+
 function createTask(){
 
 }
@@ -17,5 +36,5 @@ function viewNotConcludedTasks(){
 }
 
 function completeTask(){
-    
+
 }

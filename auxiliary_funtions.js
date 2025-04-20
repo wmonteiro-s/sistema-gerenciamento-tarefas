47c6068157a8cd189724507e46f24380fe32dead
Wesley Monteiro <wmonteiro873@gmail.com>
2025-04-20 02:36:44 -0300

    :sparkles: feat: add the create tasks function

--- auxiliary_funtions.js
+++ auxiliary_funtions.js
@@ -1,4 +1,7 @@
 import {readFile, writeFile} from 'fs/promises'
+import PromptSync from 'prompt-sync'
+const prompt = PromptSync()
+const log = console.log
 
 const FILE_NAME = 'tasks.json'
 
@@ -19,9 +22,30 @@ async function saveTasks(task) {
     }
 }
 
-function createTask(){
+function generateNewID(tasks){
+    const ids = tasks.map(task => task.id)
 
+    const descendingIds = ids.sort((a, b) => b - a)
+
+    return ids.length ? ++descendingIds[0] : 1
 }
+
+async function createTask(){
+    const title = prompt('Insira um título para a tarefa: ')
+    const description = prompt('Insira uma descrição para a tarefa: ')
+    const tasks = await loadTasks()
+    const newTask = {
+        id: generateNewID(tasks),
+        title: title,
+        description: description,
+        completed: false
+    }
+    tasks.push(newTask)
+    await saveTasks(tasks)
+    log('Tarefa criada com sucesso!')
+}
+
+await createTask()
 
 function viewAllTasks(){
 

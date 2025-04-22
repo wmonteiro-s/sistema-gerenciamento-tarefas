707f4622b4107fd314685f32011ab865fe8c3dfb
Wesley Monteiro <wmonteiro873@gmail.com>
2025-04-21 23:00:11 -0300

    :recycle: refactor: extract responsibilities from the principal, add option validation and improve menu display flow

--- main.js
+++ main.js
@@ -7,10 +7,59 @@ async function main(){
     log('Olá, seja muito bem vindo(a) ao Gerenciador de Tarefas!\n')
     
     let control = true
-    while (control) {
-        const start = prompt('Gostaria de ver o MENU DE OPÇÕES (S/N)? ').toLowerCase()
-        if (start === 's' || start === 'sim') { 
-            log(`
+    let firstTime = true
+    do {
+        let menu
+        let option
+
+        if(firstTime) {
+            log(showMenu())
+            option = prompt('Selecione uma opção: ')
+        }
+        else {
+            menu = prompt('Deseja ver o MENU DE OPÇÕES (S/N)? ').toLowerCase()
+            if(menu !== 's' && menu !== 'sim') {
+                option = prompt('Selecione uma opção: ')
+            } else {
+                log(showMenu())
+                option = prompt('Selecione uma opção: ')
+            }
+        }
+
+        const validOptions = ['1', '2', '3', '4', '5', '6']
+        
+        if(validOptions.includes(option)) control = await chooseOption(option)
+        else log('Opção Inválida')
+
+        firstTime = false
+    } while (control)
+}
+
+async function chooseOption(option){
+    switch(option){
+        case '1':
+            await createTask()
+            return true
+        case '2':
+            await viewAllTasks()
+            return true
+        case '3':
+            await viewConcludedTasks()
+            return true
+        case '4':
+            await viewNotConcludedTasks()
+            return true
+        case '5':
+            await concludeTask()
+            return true
+        case '6':
+            log('Encerrando...')
+            return false
+    }
+}
+
+function showMenu(){
+    return `
  -------------------MENU---------------------
 |1 - Criar uma nova tarefa                   |
 |2 - Visualizar todas as tarefas             |
@@ -18,36 +67,7 @@ async function main(){
 |4 - Visualizar apenas tarefas NÃO concluídas|
 |5 - Concluir uma tarefa                     |
 |6 - Sair                                    |
- --------------------------------------------`)
-            const option = prompt(': ')
-            switch(option){
-                case '1':
-                    await createTask()
-                    break
-                case '2':
-                    await viewAllTasks()
-                    break
-                case '3':
-                    await viewConcludedTasks()
-                    break
-                case '4':
-                    await viewNotConcludedTasks()
-                    break
-                case '5':
-                    await concludeTask()
-                    break
-                case '6':
-                    control = false
-                    log('Encerrando...')
-                    break
-                default:
-                    log('Opção inválida')
-            }
-        } else {
-            control = false
-            log('Encerrando...')
-        }
-    }
+ --------------------------------------------`
 }
 
 await main()
